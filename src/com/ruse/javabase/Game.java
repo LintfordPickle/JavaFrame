6191db3328e1746ec1849af58ad66584e72abc54
LintfordPickle <jonnohampson@gmail.com>
2016-07-06 19:54:09 +0200

    added self contained window creation method to JavaBase.java

--- src/com/ruse/javabase/Game.java
+++ src/com/ruse/javabase/Game.java
@@ -5,6 +5,19 @@ import com.ruse.javabase.input.Input;
 
 public class Game extends JavaBase {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Game() {
+		super("untitled");
+	}
+	
+	public Game(String pWindowTitle) {
+		super(pWindowTitle);
+	}
+	
 	public void handleInput(Input pInput){
 		
 	}
--- src/com/ruse/javabase/JavaBase.java
+++ src/com/ruse/javabase/JavaBase.java
@@ -1,11 +1,15 @@
 package com.ruse.javabase;
 
+import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import com.ruse.javabase.graphics.Art;
 import com.ruse.javabase.graphics.Bitmap;
@@ -26,6 +30,8 @@ public abstract class JavaBase extends Canvas implements Runnable {
 
 	private Thread mGameThread;
 	private boolean mIsRunning;
+	private boolean mWindowOpen;
+	private String mWindowTitle;
 
 	private Display mDisplay;
 	private Input mInput;
@@ -41,7 +47,8 @@ public abstract class JavaBase extends Canvas implements Runnable {
 	// Constructor
 	// ---------------------------------------
 
-	public JavaBase() {
+	public JavaBase(String pWindowTitle) {
+		mWindowTitle = pWindowTitle;
 		width = ConstantsTable.WINDOW_WIDTH * ConstantsTable.WINDOW_SCALE;
 		height = ConstantsTable.WINDOW_HEIGHT * ConstantsTable.WINDOW_SCALE;
 
@@ -66,6 +73,28 @@ public abstract class JavaBase extends Canvas implements Runnable {
 	// Methods
 	// ---------------------------------------
 
+	public void openWindow(){
+		if(mWindowOpen) return;
+		
+		
+		JFrame lFrame = new JFrame(mWindowTitle);
+
+		JPanel lPanel = new JPanel(new BorderLayout());
+		lPanel.add(this, BorderLayout.CENTER);
+
+		lFrame.setContentPane(lPanel);
+		lFrame.pack();
+		lFrame.setLocationRelativeTo(null);
+		lFrame.setResizable(true);
+		lFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		lFrame.setVisible(true);
+		
+		mWindowOpen = true;
+
+		start();
+		
+	}
+	
 	@Override
 	public void run() {
 
@@ -156,7 +185,5 @@ public abstract class JavaBase extends Canvas implements Runnable {
 		}
 
 	}
-
-	
 
 }
