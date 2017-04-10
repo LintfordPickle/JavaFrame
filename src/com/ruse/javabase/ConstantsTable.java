a48fa819ed549e3e2b0d64490e66f8386ff2fe20
LintfordPickle <jonnohampson@gmail.com>
2017-04-10 13:21:26 +0200

    removed unused classes

--- src/com/ruse/javabase/ConstantsTable.java
+++
@@ -1,9 +0,0 @@
-package com.ruse.javabase;
-
-public class ConstantsTable {
-
-	public static final int WINDOW_WIDTH = 320;
-	public static final int WINDOW_HEIGHT = 240;
-	public static final int WINDOW_SCALE = 2;
-	
-}
